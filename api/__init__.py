6b4e642945c11de3a062e41d68e04f3c1b2e0a5f
hexiaoyu <xiaoyu.he@capitalonline.net>
2021-12-31 16:32:08 +0800

    接口装饰器，实现序列化数据

--- api/__init__.py
+++ api/__init__.py
@@ -6,7 +6,19 @@ def api_deco(api_msg):
         def deco(*args, **kwargs):
             obj = args[0]
             req = args[1]
+            obj_serializer = None  # 不传递参数时，不会进行序列化
+            if obj.serializer_class:  # 未实现get_serializer_class，默认使用self.serializer_class,list_route指定
+                if req.method == 'GET':  # GET 方法传递参数
+                    obj_serializer = obj.get_serializer(
+                        data=req.GET)
+                else:
+                    obj_serializer = obj.get_serializer(
+                        data=req.data)
 
+            if not obj_serializer or obj_serializer.is_valid():  # 不指定serializer_class不会进行序列化
+                if obj_serializer:
+                    serializer_data = obj_serializer.data
+                    kwargs['serializer_data'] = serializer_data
 
             res = func(*args, **kwargs)
 
